2ac5f4ae82c8f9ba3b18b99af9fb87388adb4d07
mancap314 <manuel.capel82@gmail.com>
2023-03-16 23:45:32 +0100

    internal api server and test fixed

--- app/server.js
+++ app/server.js
@@ -3,10 +3,10 @@ var app = express();
 
 var fetcher = require("../src/fetcher");
 
-app.get("/etherscanContractConsumption", function(req, res) {
+app.get("/etherscanContractConsumption", async (req, res) => {
     var contractAddress = req.query.contractAddress;
     var gasConsumption = fetcher.getEtherscanData(contractAddress);
-    res.send(gasConsumption); // JSON.stringify() or something like that?
+    res.json(gasConsumption);
 });
 
 app.listen(3000);
--- test/fetcher.js
+++ test/fetcher.js
@@ -5,7 +5,8 @@ var testAddress = "0x283af0b28c62c092c9727f1ee09c02ca627eb7f5"
 
 describe("Fetching data from etherscan", () => {
     it(`Fetching for address ${testAddress}`, (done) => {
-        fetcher.getEtherscanData(testAddress);
+        var etherscan_gas_consumption = fetcher.getEtherscanData(testAddress);
+        console.log(etherscan_gas_consumption);
         done();
     })
 })
